8bf0429b28e1a81b1f62340ff273d1654b50ab1a
Gary H <26419401+Gary-H9@users.noreply.github.com>
2024-03-14 16:51:45 +0000

    WIP - initial configuration of handler.py

--- src/var/task/handler.py
+++ src/var/task/handler.py
@@ -1,52 +1,34 @@
-import json
 import os
-import subprocess
 from datetime import datetime
 
 import boto3
-import botocore.exceptions
 
 s3_client = boto3.client("s3")
-sns_client = boto3.client("sns")
-scan_time = datetime.now().isoformat()
+sm_client = boto3.client("secretsmanager")
+
+transfer_time = datetime.now().isoformat()
 
 
-def run_command(command):
-    result = subprocess.run(  # pylint: disable=subprocess-run-check
-        command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-    )
-    return (
-        result.returncode,
-        result.stdout.decode("utf-8"),
-        result.stderr.decode("utf-8"),
-    )
-
-# below kept as a placeholder to be edited
-
-def handler(event, context):  # pylint: disable=unused-argument
+def handler(event, context):
     print("Received event:", event)
-    try:
-        mode = os.environ.get("MODE")
-        if mode == "definition-upload":
-            definition_upload()
-        elif mode == "scan":
-            definition_download()
-            scan(event)
-        else:
-            raise ValueError(f"Invalid mode: {mode}")
-    except ValueError as e:
-        print(f"Configuration Error: {e}")
-        return {"statusCode": 400, "body": json.dumps({"message": str(e)})}
-    except botocore.exceptions.ClientError as e:
-        print(f"AWS Client Error: {e}")
-        return {"statusCode": 500, "body": json.dumps({"message": "AWS service error"})}
-    except Exception as e:  # pylint: disable=broad-except
-        print(f"Unexpected Error: {type(e).__name__}, {e}")
-        return {
-            "statusCode": 500,
-            "body": json.dumps({"message": "An unexpected error occurred"}),
-        }
-    return {
-        "statusCode": 200,
-        "body": json.dumps({"message": "Operation completed successfully"}),
-    }
+    print("Received context:", context)
+
+    object_key = event["Records"][0]["s3"]["object"]["key"]
+
+    supplier, file_name = object_key.split("/")[:2]
+    print(f"Supplier: {supplier}")
+    print(f"File name: {file_name}")
+
+    target_bucket_name = sm_client.get_secret_value(
+        SecretId=f"ingestion/sftp/{supplier}/target-bucket"
+    )["SecretString"]
+
+    # Move the file to the target bucket in another account
+    copy_source = {"Bucket": os.environ["PROCESSED_BUCKET_NAME"], "Key": object_key}
+    s3_client.copy_object(
+        Bucket=target_bucket_name, CopySource=copy_source, Key=object_key
+    )
+    s3_client.delete_object(
+        Bucket=os.environ["PROCESSED_BUCKET_NAME"], Key=object_key
+    )
+    print("File moved to target and tagged")
