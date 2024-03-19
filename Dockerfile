88918fa0363a2f608a5afc918dd0d2dbef21ef6e
Gary H <26419401+Gary-H9@users.noreply.github.com>
2024-03-19 15:15:35 +0000

    Working with Secrets Manager

--- Dockerfile
+++ Dockerfile
@@ -4,17 +4,9 @@ FROM public.ecr.aws/lambda/python:3.12@sha256:1d922f123370801843aad18d0911759c55
 
 LABEL org.opencontainers.image.vendor="Ministry of Justice" \
       org.opencontainers.image.authors="Analytical Platform (analytical-platform@digital.justice.gov.uk)" \
-      org.opencontainers.image.title="Ingestion Transfer" \
-      org.opencontainers.image.description="Ingestion transfer image for Analytical Platform" \
+      org.opencontainers.image.title="Ingestion Notify" \
+      org.opencontainers.image.description="Ingestion notify image for Analytical Platform" \
       org.opencontainers.image.url="https://github.com/ministryofjustice/analytical-platform"
-
-RUN microdnf update \
-    && microdnf install --assumeyes \
-         clamav-0.103.9-1.amzn2023.0.2.x86_64 \
-         clamav-update-0.103.9-1.amzn2023.0.2.x86_64 \
-         clamd-0.103.9-1.amzn2023.0.2.x86_64 \
-         tar-2:1.34-1.amzn2023.0.4.x86_64 \
-    && microdnf clean all
 
 COPY --chown=nobody:nobody --chmod=0755 src/var/task/ ${LAMBDA_TASK_ROOT}
 
--- src/var/task/handler.py
+++ src/var/task/handler.py
@@ -2,6 +2,7 @@ import os
 from datetime import datetime
 
 import boto3
+from botocore.exceptions import ClientError
 
 s3_client = boto3.client("s3")
 sm_client = boto3.client("secretsmanager")
@@ -10,11 +11,7 @@ transfer_time = datetime.now().isoformat()
 
 
 def handler(event, context):
-    print("Received event:", event)
-    print("Received context:", context)
-
     object_key = event["Records"][0]["s3"]["object"]["key"]
-
     supplier, file_name = object_key.split("/")[:2]
     print(f"Supplier: {supplier}")
     print(f"File name: {file_name}")
@@ -23,12 +20,20 @@ def handler(event, context):
         SecretId=f"ingestion/sftp/{supplier}/target-bucket"
     )["SecretString"]
 
-    # Move the file to the target bucket in another account
     copy_source = {"Bucket": os.environ["PROCESSED_BUCKET_NAME"], "Key": object_key}
-    s3_client.copy_object(
-        Bucket=target_bucket_name, CopySource=copy_source, Key=object_key
-    )
+
+    try:
+        s3_client.copy_object(
+            Bucket=target_bucket_name, CopySource=copy_source, Key=object_key
+        )
+        print(f"Successfully copied {object_key} to {target_bucket_name}")
+    except ClientError as e:
+        print(f"Error copying object: {e}")
+        return
+
+    # attempting to wrap this is try/except failed to delete
     s3_client.delete_object(
         Bucket=os.environ["PROCESSED_BUCKET_NAME"], Key=object_key
     )
-    print("File moved to target and tagged")
+    print(f"Successfully deleted {object_key} from {os.environ['PROCESSED_BUCKET_NAME']}")
+    print("handler.py completed successfully!")
